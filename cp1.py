357c6615252301610b1a987d247b517df7a661d4
Victoria Johnston <1victoriajohnston@gmail.com>
2016-02-23 13:56:07 -0500

    creating cp1.py functions

--- cp1.py
+++ cp1.py
@@ -13,7 +13,7 @@ start = '1'
 
 print nfa
 
-def modify_epsilon_transitions(nfa):
+def modify_epsilon_transitions():
 	# create a new nfa with transitions from each symbol in alphabet
 	for state in nfa:
 		# create &*
@@ -26,14 +26,32 @@ def modify_epsilon_transitions(nfa):
 			# remove & transition
 			nfa[state].pop('&',None)
 
+def add_next_states(childstate,symbol,transitions):
+	nextstates = []
+	if symbol in nfa[childstate]:
+		if isinstance(nfa[childstate][symbol],list):
+			for nextstate in nfa[childstate][symbol]:
+				nextstates.append(nextstate)
+		else:
+			nextstates.append(nfa[childstate][symbol])
+		while (len(nextstates)):
+			checkstate = nextstates.pop()
+			for state in nfa[checkstate]['&*']:
+				if state not in transitions[symbol]:
+					transitions[symbol].append(state)
 
+def add_epsilon_transitions(childstate,symbol,transitions):
+	if len(nfa[childstate]['&*']) > 1:
+		for estate in nfa[childstate]['&*']:
+			if estate not in transitions[symbol]:
+				transitions[symbol].append(estate)
 
 dfa = {}
 queue = [start]
 oldstates = []
 count = 0
 
-modify_epsilon_transitions(nfa)
+modify_epsilon_transitions()
 
 # stay in while until there are no more states to add
 while (len(queue)):
@@ -51,31 +69,33 @@ while (len(queue)):
 			# check if the symbol is in the nfa
 			print symbol
 			print childstate
-			if symbol in nfa[childstate]:
+			#if symbol in nfa[childstate]:
 				# check if the symbol is a list
-				if isinstance(nfa[childstate][symbol],list):
-					# if a symbol maps to a list of states, add each state to the transition
-					for s in nfa[childstate][symbol]:
-						states_to_check.append(s)
-				else:
-					states_to_check.append(nfa[childstate][symbol])
-				print states_to_check
-				print nfa
-				while (len(states_to_check)):
-					state = states_to_check.pop()
+			#	if isinstance(nfa[childstate][symbol],list):
+			#		# if a symbol maps to a list of states, add each state to the transition
+			#		for s in nfa[childstate][symbol]:
+			#			states_to_check.append(s)
+			#	else:
+			#		states_to_check.append(nfa[childstate][symbol])
+			#	print states_to_check
+			#	print nfa
+			#	while (len(states_to_check)):
+			#		state = states_to_check.pop()
 					# add &* transitions to transitions
-					for s in nfa[state]['&*']:
-						if s not in transitions[symbol]:
-							print "Push " + s
-							transitions[symbol].append(s)
+			#		for s in nfa[state]['&*']:
+			#			if s not in transitions[symbol]:
+			#				print "Push " + s
+			#				transitions[symbol].append(s)
+			add_next_states(childstate,symbol,transitions)
 			if not isinstance(newstate,list):
+				add_epsilon_transitions(childstate,symbol,transitions)
 				#if newstate in nfa:
-				if len(nfa[childstate]['&*']) > 1:
-					for s in nfa[childstate]['&*']:
+			#	if len(nfa[childstate]['&*']) > 1:
+			#		for s in nfa[childstate]['&*']:
 		#		if symbol in nfa[childstate]:
-						if s not in transitions[symbol]:
-							print "push " + s
-							transitions[symbol].append(s)
+			#			if s not in transitions[symbol]:
+			#				print "push " + s
+			#				transitions[symbol].append(s)
 				#print transitions[symbol]
 	
 	if isinstance(newstate,list):
